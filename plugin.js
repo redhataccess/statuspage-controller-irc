1fc78283553219aff693747825a091a3d3069aca
Jared Sprague <jared@caramelcode.com>
2017-06-04 22:31:57 -0400

    initial impl

--- plugin.js
+++ plugin.js
@@ -1,4 +1,30 @@
 var irc = require('irc');
-var client = new irc.Client('irc.devel.redhat.com', 'statusbot', {
-    channels: ['#customer-platform'],
-});
+
+var IrcPlugin = function (config) {
+    config = config || {};
+
+    var self = this;
+
+    self.config = {
+        host: config.host,
+        nick: config.nick,
+        channels: config.channels,
+    };
+
+    self.client = new irc.Client(self.config.host, self.config.nick, {
+        channels: self.config.channels,
+    });
+
+    self.hookStatusChange = function (component, status, violation) {
+        self.config.channels.forEach(function (channel) {
+            var msg = "status.redhat.com changed status of component " + component.name + ": " + status;
+            console.log('[IRC Plugin] sending irc message: ', msg);
+            self.client.say(channel, msg);
+        });
+    }
+
+};
+
+
+
+module.exports = IrcPlugin;
